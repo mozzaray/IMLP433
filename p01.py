24de578e1944b0bc06b7419a318c2575b2772565
楊凡萱ANNIEYANG <annieyang@mitac.com.tw>
2025-01-17 15:37:27 +0800

    upload exercise ~1-5

---
+++ p01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 11:28:36 2025
+
+@author: annieyang
+"""
+
+import Mytime
+
+print(Mytime.GetTime("%Y-%m-%d %H:%M:%S"))
+
+t1 = 777
+
+print(Mytime.t1)
+print(t1)
